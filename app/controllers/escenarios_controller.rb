eda8181fe721bb55f58834de1c8d2ad2c3d51102
Iratxe <iratxego@ucm.es>
2015-06-04 09:45:18 +0200

    Se despliega el escenario

--- app/controllers/escenarios_controller.rb
+++ app/controllers/escenarios_controller.rb
@@ -19,26 +19,23 @@ class EscenariosController < ApplicationController
   def new
     @escenario_ideal_id = params[:id_escenario_ideal] 
     @user_id = params[:current_user]
-    @escenario_ideal_script = [:script]
-    @escenario_ideal_mv = [:mv]
-    @escenario = Escenario.create(:id_escenario_ideal => @escenario_ideal_id, :id_user => @user_id)
-     #Que un usuario no pueda lanzar un escenario 2 veces
-    #Mostrar escenario lanzado/no lanzado
-    #@escenario = EscenarioIdeal.find(params[:id])
-    #@current_escenario = Escenario.create(:id_escenario_ideal => @escenario_ideal.id, :id_user => current_user.id)
-    #system('su oneadmin -c "/var/lib/one/crea.sh ES001 #{@escenario_ideal_script} #{@escenario_ideal_mv}&"')
-    #system('su oneadmin -c "/var/lib/one/crea.sh ES001 2:1524:2 router.tmpl:victima.tmpl &"')
-    #@id_escenario_ideal
-    #Escenario.new(:id_escenario_ideal => '', :id_user => 'current_user.id')
-    #string = %x{ender.dacya.ucm.es/var/lib/one/crea.sh}
-    #ender.dacya.ucm.es/var/lib/one/crea.sh
-    #if string.include? "FIN"
-    #Mostrar el popup en la vista del escenario
+    @escenario_ideal_script = params[:script]
+    @escenario_ideal_mv = params[:mv]
+    if !des=File.exist?("/var/lib/one/escenarios/"+@escenario_ideal_id+"")
+      @escenario = Escenario.create(:id_escenario_ideal => @escenario_ideal_id, :id_user => @user_id)
+      @ruta="su oneadmin -c \"/var/lib/one/crea.sh "+@escenario.id+" "+@escenario_ideal_script+" "+@escenario_ideal_mv+" &\""
+      des=File.exist?("/var/lib/one/escenarios/"+@escenario.id+"/exito")  
+      system(@ruta)
+    end
+    
+     #system(@ruta)
+   
+    if des=File.exist?("/var/lib/one/escenarios/"+@escenario_ideal_id+"/exito")
     flash[:success] = "Es escenario se ha desplegado con éxito. ¡Descárgate los 
     #{view_context.link_to("certificados", "/prueba.zip")}, y empieza a practicar!"
     #/prueba.zip
     ##{Rails.root}/var/lib/one/escenarios/ES002/ctr.zip
-    #end
+    end
   end
 
   # GET /escenarios/1/edit
